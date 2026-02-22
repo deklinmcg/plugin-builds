1debf3cc03cdcdcb10645bc43ec603e8eee2cf18
Declan <deklin@gmail.com>
2026-02-22 11:51:27 +0400

    Add WebViewTest plugin

---
+++ plugin/WebViewTest/Source/PluginEditor.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+class WebViewTestAudioProcessorEditor : public juce::AudioProcessorEditor
+{
+public:
+    explicit WebViewTestAudioProcessorEditor (WebViewTestAudioProcessor&);
+    ~WebViewTestAudioProcessorEditor() override;
+    void paint (juce::Graphics&) override {}
+    void resized() override { webView.setBounds (getLocalBounds()); }
+
+private:
+    WebViewTestAudioProcessor& audioProcessor;
+    juce::WebBrowserComponent webView;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (WebViewTestAudioProcessorEditor)
+};
